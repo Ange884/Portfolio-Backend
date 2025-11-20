83bbadc9ebb1483d2fc8dc7ae0d94440246f5106
Ange <ishimwenzizaangell@gmail.com>
2025-11-20 15:23:38 +0200

    .

--- Server.js
+++ Server.js
@@ -13,10 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .catch(err => console.log(err));
+
 
 // Routes
 
